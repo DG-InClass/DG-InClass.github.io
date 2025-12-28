a1ad8502fd7640aa57fcc25c3ef225f1642caca7
Dan Gilleland <dgilleland@nait.ca>
2025-12-28 08:09:42 -0700

    Add Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,26 +2,37 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://dg-inclass.github.io',
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    site: 'https://dg-inclass.github.io',
+
+    integrations: [
+        starlight({
+            title: 'My Docs',
+            customCss: [
+                // Path to your Tailwind base styles:
+                './src/styles/global.css',
+            ],
+            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/dg-inclass/dg-inclass.github.io' }],
+            sidebar: [
+                {
+                    label: 'Guides',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: 'Example Guide', slug: 'guides/example' },
+                    ],
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' },
+                },
+            ],
+        }),
+    ],
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
